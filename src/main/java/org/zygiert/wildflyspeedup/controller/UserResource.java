920a44eb8e4a7b39f3cebee85cb72e062a10dad5
Michał Zyga <michal.zyga@softwaremill.com>
2026-05-17 16:47:35 +0200

    add logger for saving user

--- src/main/java/org/zygiert/wildflyspeedup/controller/UserResource.java
+++ src/main/java/org/zygiert/wildflyspeedup/controller/UserResource.java
@@ -6,10 +6,14 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.zygiert.wildflyspeedup.service.UserService;
 
+import java.util.logging.Logger;
+
 @Path("/users")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class UserResource {
+
+    private static final Logger LOGGER = Logger.getLogger(UserResource.class.getName());
 
     @Inject
     private UserService userService;
@@ -30,6 +34,7 @@ public class UserResource {
 
     @POST
     public Response save(UserDTO user) {
+        LOGGER.info("Saving user: " + user);
         return Response.ok(userService.save(user)).build();
     }
 
